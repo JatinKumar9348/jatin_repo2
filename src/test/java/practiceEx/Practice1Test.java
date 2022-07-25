23fd8e03e869aeeecb1df50a110082d46302d790
JatinKumar9348 <109574862+JatinKumar9348@users.noreply.github.com>
2022-07-25 13:12:54 +0530

    Update Practice1Test.java

--- src/test/java/practiceEx/Practice1Test.java
+++ src/test/java/practiceEx/Practice1Test.java
@@ -5,7 +5,7 @@ public class Practice1Test {
 	public static void main(String[] args) {
      int a=10;
      int b=20;
-    // System.out.println("value of a+b : "+a+b);
+     System.out.println(a+b);
      int c=a+b;
      System.out.println(c);
 	}
